cb897232e7a3acae5b742cc1a2f93e234f4eb779
Tim Dresser <tdresser@chromium.org>
2018-11-15 11:34:13 -0800

    Working on options page.

--- options.js
+++ options.js
@@ -1,43 +1,58 @@
-(function() {
-  document.getElementById("fieldset").addEventListener("change", (e) => {
-    switch(e.target.id) {
+(async function() {
+  function applyState() {
+    document.getElementById("textarea").value = state.eval;
+    switch(state.selected) {
     case 'log':
       window.updatingLink = (state, a) => {
         console.log("state: " + state);
         console.log(a);
       }
       break;
+    case 'highlight':
+      window.updatingLink = (state, a) => {
+        console.log("highlight: state: " + state);
+        console.log(a);
+      }
+      break;
     case 'eval':
-    case 'textarea':
-      eval(document.getElementById("textarea").value);
+      eval(state.eval);
       break;
     default:
-      throw "invalid form element item id " + e.target.id;
+      throw "invalid selected state " + state.selected;
     }
+  }
+
+  async function getState() {
+    return new Promise(resolve => {
+      chrome.storage.sync.get(['state'], result => resolve(result));
+    });
+  }
+
+  // TODO: should this be fire & forget, or not?
+  // TODO: maybe throttle this?
+  function setState() {
+    return new Promise(resolve => {
+      chrome.storage.sync.set(['state'], state);
+    });
+  }
+
+  let state = await getState();
+  if (!("selected" in state) || !("eval" in state)) {
+    state = {
+      selected: "log",
+      eval: "",
+    }
+  }
+
+  applyState();
+
+
+  document.getElementById("fieldset").addEventListener("change", (e) => {
+    if (e.target.id == "textarea")
+      state.eval = document.getElementById("textarea").value;
+    else
+      state.selected = e.target.id;
+
+    applyState();
   });
 })();
-
-/*
-function save(key) {
-  if (timer)
-    clearTimeout(timer);
-
-  timer = setTimeout(function() {
-    timer = null;
-    var data = {};
-    data[key] = document.getElementById(key).value;
-    chrome.storage.sync.set(data, function() {});
-  }, 100);
-};
-
-chrome.storage.sync.get({
-  custom_callback_code: null,
-}, function(items) {
-  var content = document.getElementById('content');
-  let textarea = document.createElement('textarea');
-  textarea.onkeydown = function() {
-    save('custom_callback_code');
-  };
-  content.append(textarea);
-});
-*/
